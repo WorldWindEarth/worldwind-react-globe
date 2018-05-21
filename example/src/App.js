6460b00d30597f161d865cf3348af0f1beaeb20b
Bruce Schubert <bruce@emxsys.com>
2018-05-21 06:57:38 -0700

    Updated example.

--- example/src/App.js
+++ example/src/App.js
@@ -1,17 +1,56 @@
 import React, { Component } from 'react'
-
 import Globe from 'worldwind-react-globe'
+import WorldWind from '@nasaworldwind/worldwind';
+
+import './App.css'
 
 export default class App extends Component {
+        
+    constructor(props) {
+        super(props);
+        
+        // Holds a reference to the Globe component after mounting
+        this.globeRef = React.createRef();
+        
+        // Define the WorldWind base URL for image resources.
+        // As an alternative, you can copy the images folder from 
+        // https://files.worldwind.arc.nasa.gov/artifactory/web/0.9.0/
+        // to the root of your web app (e.g., to public)
+        WorldWind.configuration.baseUrl = 'https://files.worldwind.arc.nasa.gov/artifactory/web/0.9.0/';
+    }        
+         
+    componentDidMount() {
+        // Get the Globe component with the WorldWindow after mounting
+        const globe = this.globeRef.current;
+        
+        // Define layers to be added to the globe.
+        const layerConfig = [
+            {layer: new WorldWind.BMNGLandsatLayer(),
+                options: {category: "base", enabled: true}},
+            {layer: new WorldWind.CompassLayer(),
+                options: {category: "setting", enabled: false}},
+            {layer: new WorldWind.CoordinatesDisplayLayer(globe.wwd),
+                options: {category: "setting", enabled: true}},
+            {layer: new WorldWind.ViewControlsLayer(globe.wwd),
+                options: {category: "setting", enabled: true}},
+            {layer: new WorldWind.StarFieldLayer(),
+                options: {category: "setting", enabled: true, displayName: "Stars"}},
+        ];
+        // Add the layers to the globe
+        layerConfig.forEach(config => globe.addLayer(config.layer, config.options));
+    }
+    
     render () {
-        const divStyle = {
-            width: '100vw',
-            height: '100vh'
-        };
-
+        // Create a Globe in a div that fills the viewport.
+        // 
+        // The id prop defines the element id for the Globe's canvas and is required.
+        // The projection prop is optional, 3D is the default.
         return (
-            <div style={divStyle}>
-                <Globe id="globe-canvas" />
+            <div className="fullsize">
+                <Globe 
+                    ref={this.globeRef}
+                    id="globe-canvas"
+                    projection="3D"/>
             </div>
         )
     }
