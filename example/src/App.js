cfcb8cbc821480b6380e147aaf02df4dd326a82d
Bruce Schubert <bruce@emxsys.com>
2018-05-23 15:47:05 -0700

    Updated App to use simplified layer props in worldwind-react-globe 0.4.0.

--- example/src/App.js
+++ example/src/App.js
@@ -4,25 +4,15 @@ import Globe from 'worldwind-react-globe'
 import './App.css'
 
 export default class App extends Component {
-
   render() {
     // See Globe.defaultLayers for a list of layer string identifiers
-    const layers = [{
-        layer: "Sentinal2 with Labels", // partial names are ok
-        options: {category: "base", enabled: true}
-      }, {
-        layer: "Compass",
-        options: {category: "setting", enabled: true}
-      }, {
-        layer: "Coordinates",
-        options: {category: "setting", enabled: true}
-      }, {
-        layer: "View Controls",
-        options: {category: "setting", enabled: true}
-      }, {
-        layer: "Stars",
-        options: {category: "setting", enabled: false, displayName: "Stars"}
-      }
+    const layers = [
+      "Sentinal2 with Labels", // partial names are OK
+      "Compass",
+      "Coordinates",
+      "View Controls",
+      "Atmosphere",
+      "Stars"
     ];
     return (
       <div className="fullscreen">
