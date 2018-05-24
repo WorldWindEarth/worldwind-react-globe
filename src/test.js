af6a3846c2dc1b0ad741f4eee8ce791e23f5a528
Bruce Schubert <bruce@emxsys.com>
2018-05-24 09:44:41 -0700

    Added some basic globe and layer unit tests.

--- src/test.js
+++ src/test.js
@@ -1,7 +1,61 @@
+/* 
+ * Copyright (c) 2018 Bruce Schubert.
+ * The MIT License
+ * http://www.opensource.org/licenses/mit-license
+ */
+import React, {Component} from 'react';
+import TestRenderer from 'react-test-renderer';
 import Globe from './components/Globe';
+import 'webgl-mock';
 
-describe('Globe', () => {
-    it('is truthy', () => {
-        expect(Globe).toBeTruthy();
-    });
+/* global WorldWind, expect */
+
+/**
+ * Mock for WorldWindow.createContext() 
+ * @param {canvas} canvasIgnored
+ * @returns {}
+ */
+WorldWind.WorldWindow.prototype.createContext = function (canvasIgnored) {
+  let mockCanvas = new HTMLCanvasElement(500, 500);
+  let gl = mockCanvas.getContext('webgl');
+  return gl;
+};
+
+/**
+ * Add a canvas to the DOM to satisfy WorldWindow constructor
+ */
+beforeEach(() => {
+  document.body.innerHTML = '<canvas id="test-canvas">Test Canvas</canvas>';
 });
+
+/**
+ * Basic globe tests
+ */
+describe('Globe', () => {
+  it('is valid', () => {
+    const globe = TestRenderer.create(<Globe canvasId="test-canvas" />).root.instance;
+    expect(globe.state.isValid).toBeTruthy();
+  });
+
+  it('default globe has one layer', () => {
+    const globe = TestRenderer.create(<Globe canvasId="test-canvas" />).root.instance;
+    expect(globe.getLayers().length).toBe(1);
+  });
+});
+
+/**
+ * Layer management tests
+ */
+describe('Layers', () => {
+  Globe.defaultLayers.forEach(layerKey => {
+    it('add "' + layerKey + '"', () => {
+      const globe = TestRenderer.create(<Globe canvasId="test-canvas" />).root.instance;
+
+      const layer = globe.addLayer(layerKey);
+      
+      expect(globe.getLayer(layer.displayName)).toBeInstanceOf(WorldWind.Layer);
+      expect(globe.getLayer(layer.uniqueId)).toBeInstanceOf(WorldWind.Layer);
+    });
+  });
+});
+
